c02045cd5b12afd50dc28b5c92829a4b184406e4
Jan Renz <jan.renz@hpi.de>
2016-03-02 14:08:53 +0100

    konfetti

--- wheel.js
+++ wheel.js
@@ -120,7 +120,12 @@ function checkStartDrag(e) {
 
         //statusLabel.innerHTML = "des dreht sisch doch noch...";
     }else{
-        wheel.body.angularVelocity = Math.max(wheel.body.angularVelocity, 1.5);
+      if (wheel.body.angularVelocity < 0 && wheel.body.angularVelocity>-1.5){
+        //wheel.body.angularVelocity = -1.5;
+      }else if (wheel.body.angularVelocity > 0 && wheel.body.angularVelocity<1.5){
+        //wheel.body.angularVelocity = 1.5;
+      }
+
     }
 }
 
@@ -195,8 +200,23 @@ function initPhysics() {
     world.addBody(mouseBody);
 }
 
+function spawnPartices() {
+    for (var i = 0; i < 250; i++) {
+        var p0 = new Point(viewCenterX+100, viewCenterY );
+        var p1 = new Point(viewCenterX+100, 0);
+        var p2 = new Point(Math.random() * viewWidth, Math.random() * viewCenterY);
+        var p3 = new Point(Math.random() * viewWidth, viewHeight + 64);
+        particles.push(new Particle(p0, p1, p2, p3));
+    }
+}
 
 function update() {
+  particles.forEach(function(p) {
+    p.update();
+    if (p.complete) {
+        particles.splice(particles.indexOf(p), 1);
+    }
+  });
     //setColorForBg( wheel.currentSegment());
     // console.log('wheelSpinning' + wheelSpinning);
     // console.log('wheelStopped' + wheelStopped);
@@ -211,6 +231,9 @@ function update() {
         wheel.body.angularVelocity = 0;
         var color = ctx.getImageData((1920/2)+100,200,1,1).data;
         $('body').css('background-color', 'rgb('+color[0]+','+color[1]+','+color[2]+')');
+        if (color[0]==255 && color[1]==255 && color[2]==255){
+          spawnPartices();
+        }
         setTimeout(function(){
             $('body').css('background-color', '#ffffff');
         }, 5000);
@@ -222,9 +245,18 @@ function update() {
 
 function draw() {
     // ctx.fillStyle = '#fff';
-    ctx.clearRect(0, 0, 1920, 100);//could narrored down to just the arror
+    if (particles.length > 0){
+        ctx.clearRect(0, 0, 1920, 1200);
+    }else{
+        ctx.clearRect(0, 0, 1920, 100);
+    }
+  //could narrored down to just the arror
     wheel.draw();
     arrow.draw();
+
+    particles.forEach(function(p) {
+        p.draw();
+    });
 }
 
 
@@ -408,3 +440,95 @@ Arrow.prototype = {
         ctx.restore();
     }
 };
+/////////////////////////////
+// your reward
+/////////////////////////////
+Particle = function(p0, p1, p2, p3) {
+    this.p0 = p0;
+    this.p1 = p1;
+    this.p2 = p2;
+    this.p3 = p3;
+
+    this.time = 0;
+    this.duration = 3 + Math.random() * 2;
+    this.color =  'hsl(' + Math.floor(Math.random() * 360) + ',100%,50%)';
+
+    this.w = 10;
+    this.h = 7;
+
+    this.complete = false;
+};
+Particle.prototype = {
+    update:function() {
+        this.time = Math.min(this.duration, this.time + timeStep);
+
+        var f = Ease.outCubic(this.time, 0, 1, this.duration);
+        var p = cubeBezier(this.p0, this.p1, this.p2, this.p3, f);
+
+        var dx = p.x - this.x;
+        var dy = p.y - this.y;
+
+        this.r =  Math.atan2(dy, dx) + HALF_PI;
+        this.sy = Math.sin(Math.PI * f * 10);
+        this.x = p.x;
+        this.y = p.y;
+
+        this.complete = this.time === this.duration;
+    },
+    draw:function() {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.r);
+        ctx.scale(1, this.sy);
+
+        ctx.fillStyle = this.color;
+        ctx.fillRect(-this.w * 0.5, -this.h * 0.5, this.w, this.h);
+
+        ctx.restore();
+    }
+};
+Point = function(x, y) {
+    this.x = x || 0;
+    this.y = y || 0;
+};
+/////////////////////////////
+// math
+/////////////////////////////
+/**
+ * easing equations from http://gizma.com/easing/
+ * t = current time
+ * b = start value
+ * c = delta value
+ * d = duration
+ */
+var Ease = {
+    inCubic:function (t, b, c, d) {
+        t /= d;
+        return c*t*t*t + b;
+    },
+    outCubic:function(t, b, c, d) {
+        t /= d;
+        t--;
+        return c*(t*t*t + 1) + b;
+    },
+    inOutCubic:function(t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c/2*t*t*t + b;
+        t -= 2;
+        return c/2*(t*t*t + 2) + b;
+    },
+    inBack: function (t, b, c, d, s) {
+        s = s || 1.70158;
+        return c*(t/=d)*t*((s+1)*t - s) + b;
+    }
+};
+
+function cubeBezier(p0, c0, c1, p1, t) {
+    var p = new Point();
+    var nt = (1 - t);
+
+    p.x = nt * nt * nt * p0.x + 3 * nt * nt * t * c0.x + 3 * nt * t * t * c1.x + t * t * t * p1.x;
+    p.y = nt * nt * nt * p0.y + 3 * nt * nt * t * c0.y + 3 * nt * t * t * c1.y + t * t * t * p1.y;
+
+    return p;
+}
